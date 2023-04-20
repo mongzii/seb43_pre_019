5fbf90d845c2cf625f7d301ebec3dc12f365f4dd
moonjiyoung <umarg228@naver.com>
2023-04-20 15:55:45 +0900

    fix: toast 오류 수정

--- client/src/services/useAxios.js
+++ client/src/services/useAxios.js
@@ -1,25 +1,25 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const useAxios = (url) => {
+const useAxios = url => {
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     axios(url, {
-      method: "GET",
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         // ngrok 으로 데이터 받을 때 browser warning 스킵
         // 'ngrok-skip-browser-warning': '69420',
       },
     })
-      .then((response) => {
+      .then(response => {
         setQuestions(response.data);
         setAnswers(response.data.answers);
       })
-      .catch((err) => setError(err.message));
+      .catch(err => setError(err.message));
   }, [url]);
 
   return [questions, setQuestions, answers, setAnswers, error];
